d457e2b01a06c4a9f2fa60f798ffc08b6e17a58f
Al <mesolimbo@users.noreply.github.com>
2025-12-21 10:51:18 -0500

    Replace cow svg with png

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -17,7 +17,7 @@ export class BootScene extends Phaser.Scene {
     // Load all SVG assets
     this.load.image('ufo', 'assets/ufo.png');
     this.load.image('ufo_thrust', 'assets/ufo_thrust.png');
-    this.load.svg('cow', 'assets/cow.svg');
+    this.load.image('cow', 'assets/cow.png');
     this.load.image('barn', 'assets/barn.png');
     this.load.image('silo', 'assets/silo.png');
     this.load.image('ground', 'assets/ground.png');
